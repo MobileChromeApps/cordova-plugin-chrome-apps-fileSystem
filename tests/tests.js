f4ab8542de9cc1f3e9c01fbfd8f42861596ea008
Jason Chase <jasonpchase@gmail.com>
2015-04-04 19:44:28 -0400

    Merge auto tests from test.chrome.fileSystem.js

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* global logger */
+
 exports.defineManualTests = function(rootEl, addButton) {
   var FileReader = window.FileReader || cordova.require('cordova/plugin/FileReader');
 
@@ -126,5 +128,47 @@ exports.defineManualTests = function(rootEl, addButton) {
                                accepts: [{ mimeTypes: ['video/*'] }] };
 
     chrome.fileSystem.chooseEntry(chooseEntryOptions, chooseEntryCallback);
+  });
+};
+
+exports.defineAutoTests = function() {
+  'use strict';
+  describeExcludeChrome('chrome.fileSystem', function() {
+    // Create a file entry for testing.
+    var fileEntry = new FileEntry('filename', 'fullpath');
+
+    describe('getDisplayPath()', function() {
+      it('returns the full path of the given file entry', function() {
+        // Create the callback.
+        var getDisplayPathCallback = function(displayPath) {
+          expect(displayPath).toEqual(fileEntry.fullPath);
+        };
+
+        // Get the display path.
+        chrome.fileSystem.getDisplayPath(fileEntry, getDisplayPathCallback);
+      });
+    });
+    describe('getWritableEntry()', function() {
+      it('returns a writable file entry', function() {
+        // Create the callback.
+        var getWritableEntryCallback = function(writableFileEntry) {
+          expect(writableFileEntry).not.toBeNull();
+        };
+
+        // Get the writable file entry.
+        chrome.fileSystem.getWritableEntry(fileEntry, getWritableEntryCallback);
+      });
+    });
+    describe('isWritableEntry()', function() {
+      it('returns true', function() {
+        // Create the callback.
+        var isWritableEntryCallback = function(isWritable) {
+          expect(isWritable).toBeTruthy();
+        };
+
+        // Get whether the file entry is writable.
+        chrome.fileSystem.isWritableEntry(fileEntry, isWritableEntryCallback);
+      });
+    });
   });
 };
