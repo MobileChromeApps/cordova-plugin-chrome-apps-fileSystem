f10be74aefff5d93ba8cfeda2b783e412e93ca7f
Jason Chase <jasonpchase@gmail.com>
2015-04-09 15:54:54 -0400

    Setup jasmine helpers for auto tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/* global logger */
-
 exports.defineManualTests = function(rootEl, addButton) {
   var FileReader = window.FileReader || cordova.require('cordova/plugin/FileReader');
 
@@ -133,6 +131,9 @@ exports.defineManualTests = function(rootEl, addButton) {
 
 exports.defineAutoTests = function() {
   'use strict';
+
+  require('cordova-plugin-chrome-apps-test-framework.jasmine_helpers').addJasmineHelpers();
+
   describeExcludeChrome('chrome.fileSystem', function() {
     // Create a file entry for testing.
     var fileEntry = new FileEntry('filename', 'fullpath');
